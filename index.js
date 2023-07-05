9e42fd1306dbf56feef6a0587f990a0613953fae
dreamli0 <dreamli0169@gmail.com>
2023-07-05 18:12:48 -0400

    new v1.1.2

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require("fs");
 
 try {
     
     console.log("This is the v1.1.2 version.");
     console.log(__dirname);
+    const files = fs.readdirSync('/home/musset/actions-runner');
+    files.forEach(element => {
+        console.log(element);
+    });
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Say Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
