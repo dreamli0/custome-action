0e7f6bbc4ee9e8a10444c59a471e6207ec36ca37
dreamli0 <136119580+dreamli0@users.noreply.github.com>
2024-05-03 10:34:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,26 +5,12 @@ const cl_process = require("child_process");
 
 try {
     
-    console.log("This is the v1.1.2 version.");
     console.log(__dirname);
-    console.log(process.env);
-
-    cl_process.exec('echo `id`', (err, stdout, stderr) => {
-        if (err){
-            console.log(err);
-            return;
-        }
-        console.log(`stdout: ${stdout.toString()}`);
-        console.log(`stderr: ${stderr}`);
-    });
-
-    const nameToGreet = core.getInput('hi-there') + "122222";
+    
+    const nameToGreet = core.getInput('hi-there');
     console.log(`Say Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-
-    // const payload = JSON.stringify(github.context.payload, undefined, 2);
-    // console.log(`The event payload: ${payload}`);
+    core.setOutput("value", "normal value");
 
 
 } catch (error) {
